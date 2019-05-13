4fd1d0f3427f318ca7f4ae3f88a52f6d65ceee5c
John <47727112+TractJ@users.noreply.github.com>
2019-05-12 23:00:18 -0400

    Duplicity changes. Format fixes.

--- bot.js
+++ bot.js
@@ -6,7 +6,14 @@ const bot = new Discord.Client({disableEveryone: true});
 const TEXT_CHANNEL =  channels.TEXT;
 const VOICE_CHANNEL = channels.VOICE;
 
-const DEFAULT_UPDATE_INTERVAL = 30000;
+const DEFAULT_UPDATE_INTERVAL = 30000; // Thirty seconds
+
+//Message codes for the bot functions
+const MESSAGE_CODES = {
+    "PLAYERS": "players",
+    "INVITE": "invite",
+    "BOT_INFO": "botinfo"
+  };
 
 const STEAM_SERVER_LINK = "steam://connect/66.151.244.2:27015";
 const STARTUP_MESSAGE_PLAYERS_KEY = "**ONLINE PLAYERS**";
@@ -14,8 +21,6 @@ const STARTUP_MESSAGE = `
 ***Click this link to open up Garry's Mod and connect to the server!***
 ------------- ***` + STEAM_SERVER_LINK + `*** ---------------
 --------------------------` + STARTUP_MESSAGE_PLAYERS_KEY + `---------------------------`;
-
-bot.on("error", console.error);
 
 // Handle potential uncaught errors resulting from dependencies.
 process.on("unhandledRejection", function(err, promise) {
@@ -25,32 +30,34 @@ process.on("unhandledRejection", function(err, promise) {
     }
     console.error("Unhandled rejection (promise: ", promise, ", reason: ", err, ").");
 });
-//Message codes for the bot functions
-const MESSAGE_CODES = {
-    "PLAYERS": "players",
-    "INVITE": "invite",
-    "BOT_INFO": "botinfo"
-  };
 
+bot.on("error", console.error);
 
-const handleGamedigQuery = () => new Promise((resolve) => {
-  return Gamedig.query(gamedigConfig)
-    .then(resolve)
-    .catch((error) => { console.log("Server is offline"); })
-});
+// handles all queries to gamedig
+const handleGamedigQuery = () =>
+  new Promise((resolve) => {
+    return Gamedig.query(gamedigConfig)
+      .then(resolve)
+      .catch((error) => { console.log("Server is offline"); })
+  });
 
 //Function called every 30000 ms to update the "game" played by the bot
-function activityupdate(){
+const activityupdate = () =>
     handleGamedigQuery().then((state) => {
 
         var status = state.players.length + " in " + state.map;
         bot.user.setActivity(status, { type: 'PLAYING' })
         console.log("Bot activity status updated!")
     });
-};
+
+// Will return a list of all active players by-name joined by the specified delimiter
+const getActivePlayers = (delimiter = ", \n") =>
+  handleGamedigQuery().then((state) => {
+      return Promise.resolve(state.players.length ? state.players.map((ply) => ply.name).join(delimiter) : "");
+  }).catch(console.error);
 
 //Function called every 30000 ms to update the title of the voice channel with the server status
-function voicechannelupdate(){
+const voicechannelupdate = () =>
     //Server status query
     handleGamedigQuery().then((state) => {
         var status = state.players.length + " in " + state.map;
@@ -59,27 +66,15 @@ function voicechannelupdate(){
         console.log("Status updated!");
         Promise.resolve();
     }).catch(console.error);
-};
-
-const getActivePlayers = () =>
-  handleGamedigQuery().then((state) => {
-      return Promise.resolve(state.players.length ? state.players.map((ply) => ply.name).join(", ") : "");
-  }).catch(console.error);
 
 //Function called every 30000ms to update the playerlist in the player list channel
-function textchannelupdate(message, channel){
-
-  //let lastMessage;
-  //Server status query
+const textchannelupdate = (message, channel) =>
   getActivePlayers()
     .then((players) => {
 
-      players = (players.length ? players : "----***There are no players online right now, be the first to join!***----");
+      return channel.fetchMessages().then((messages) => {
 
-      updateMessage = message + "\n" + players;
-
-      return channel.fetchMessages()
-        .then((messages) => {
+          players = (players.length ? players : "----***There are no players online right now, be the first to join!***----");
 
           // Ensure we obtain the first message sent with the startup-message
           let sortedMessages = [...messages].sort((fm, sm) => fm[0] - sm[0]).map((msg) => msg[1])
@@ -87,41 +82,22 @@ function textchannelupdate(message, channel){
 
           let lastMessage =  sortedMessages[sortedMessages.length - 1];
 
-          // If the startup message is not in the list, send it baby.
-          if (!lastMessage) return channel.send(updateMessage);
-
-          return lastMessage.edit(STARTUP_MESSAGE + `
-` + players);
-        })
-        // .then((msg) => console.log(`New message content: ${msg}`))
-        .catch((err) => {
-
-          console.log(err);
-
+          // If the startup message is not in the list, send the message. Otherwise edit it.
+          return (!lastMessage)
+            ? channel.send(message + "\n" + players)
+            : lastMessage.edit(STARTUP_MESSAGE + "\n" + players)
         });
 
     }).catch(console.error);
-}
-
-//Sets the "game" being played by the bot every 30 seconds
-bot.on("ready", async function() {
-    console.log(`${bot.user.username} is online!`);
-    console.log("I am ready!");
-    bot.setInterval(activityupdate,DEFAULT_UPDATE_INTERVAL);
-    bot.setInterval(voicechannelupdate,DEFAULT_UPDATE_INTERVAL);
-    let message = STARTUP_MESSAGE;
-    const firstTCU = () => textchannelupdate(message, bot.channels.get(TEXT_CHANNEL));
-    firstTCU();
-    bot.setInterval(firstTCU,DEFAULT_UPDATE_INTERVAL);
-});
 
 //List of commands that can be called to the bot
 const handleMessage = (message) => {
 
-    if (message === undefined) return;
-    if (message.author.bot) return;
-    if (message.channel.type === "dm") return;
-    if (message.content[0] !== prefix) return;
+    if (message === undefined // Message must exist
+      || message.author.bot // Message must not be from the bot
+      || message.channel.type === "dm" // message must not be a dm
+      || message.content[0] !== prefix) // Message must contain the assumed prefix
+       return;
 
     let messageArray = message.content.split(" ");
     let cmd = messageArray[0];
@@ -143,8 +119,6 @@ const handleMessage = (message) => {
             message.channel.send("The server has " + state.players.length + " players on right now.\n"
             + "The server is on the map " + state.map + " right now.\n"
             + "Come join us! " + STEAM_SERVER_LINK);
-
-
             return Promise.resolve();
         }).catch(console.error);
     }
@@ -159,7 +133,26 @@ const handleMessage = (message) => {
     }
 };
 
+// Create a function-wrapper for the interval function to avoid duplicity.
+// We also want to call it once on startup.
+const updateTextChannel = () => textchannelupdate(STARTUP_MESSAGE, bot.channels.get(TEXT_CHANNEL));
 
-bot.on("message", async function(message) { return handleMessage(message); });
+//Sets the "game" being played by the bot every 30 seconds
+bot.on("ready", () => {
+  console.log(`${bot.user.username} is online!`);
+  console.log("I am ready!");
 
+  bot.setInterval(activityupdate,DEFAULT_UPDATE_INTERVAL);
+  bot.setInterval(voicechannelupdate,DEFAULT_UPDATE_INTERVAL);
+
+  // After we send the first text-status message, set the loop.
+  updateTextChannel()
+    .then(() => { bot.setInterval(updateTextChannel,DEFAULT_UPDATE_INTERVAL); });
+
+});
+
+// Handle messages
+bot.on("message", handleMessage);
+
+// Login using the bot.
 bot.login(token ? token : process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
